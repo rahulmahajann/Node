6c2e8d02edab0841a312d0a29ad8a6b8142962b4
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-21 01:47:06 +0530

    added

--- sec-4(weather-app)/1.js
+++ sec-4(weather-app)/1.js
@@ -1,9 +1,33 @@
+// WEATHER STACK API
+
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=693cd55c6f6769acc9c1728688ac0ae4&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=693cd55c6f6769acc9c1728688ac0ae4&query=delhi&units=s'
 
 request({ url: url ,json: true  },(error,response) =>{
+
+    // this is used when we are not using json:true as an object
+
     // const data = JSON.parse(response.body)
     // console.log(data.current.temperature);
+    
+
+    // handling the error
+
+    if(error){
+        console.log('unable to connect to weather api');
+    }else{
+
     console.log(response.body.current.temperature);
+    console.log(response.body.current.weather_code);
+    console.log(response.body.current.weather_icons[0]);
+
+    console.log('its currently ' + response.body.current.temperature + ' degrees out. it feels like ' 
+    + response.body.current.precip + '% precipitation here!');
+
+    console.log('current weather condition is: ' + response.body.current.weather_descriptions[0]);
+
+        
+    }
+
 })
---
+++ sec-4(weather-app)/2.js
@@ -0,0 +1,19 @@
+// MAPBOX API
+
+const request = require('request')
+
+url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/delhi.json?access_token=pk.eyJ1IjoicmFodWxtYWhhamFubiIsImEiOiJja21pNG55cXMwZG92Mm5xdndwcHNvdXlsIn0.ESrPYLF0G3EDv8Y2d_NIZw'
+
+request({url : url , json: true} , (error,response) => {
+
+    // lon --> 0
+    // lat --> 1
+
+
+    if(error){
+        console.log('unable to connect to mapbox api');
+    }else{
+    console.log('latitutde of delhi --> ' + response.body.features[0].center[1]);
+    console.log('longitude of delhi --> ' + response.body.features[0].center[0]);
+    }
+}) 
