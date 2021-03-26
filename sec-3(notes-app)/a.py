4a2f5dad97c6ae278db5b76c66ee084b1ef0568d
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-27 00:32:19 +0530

    added

--- sec-3(notes-app)/a.py
+++ sec-3(notes-app)/a.py
@@ -1,47 +1,47 @@
 # 2020 A-a  (AC)
 
-for i in range(int(input())):
-    a,b=map(int,input().split())
-    c=sorted(list(map(int,input().split())))
-    d=0
-    count=0
-    for _ in c:
-        d+=_
-        if(d<=b):
-            count+=1
+# for i in range(int(input())):
+#     a,b=map(int,input().split())
+#     c=sorted(list(map(int,input().split())))
+#     d=0
+#     count=0
+#     for _ in c:
+#         d+=_
+#         if(d<=b):
+#             count+=1
     
-    ans=[]
-    ans.append('Case #')
-    ans.append(str(i+1))
-    ans.append(': ')
-    ans.append(str(count))
-    print(''.join(ans))
+#     ans=[]
+#     ans.append('Case #')
+#     ans.append(str(i+1))
+#     ans.append(': ')
+#     ans.append(str(count))
+#     print(''.join(ans))
 
 
 # 2019 A-a  (WA)
 
-for i in range(int(input())):
-   a,b=map(int,input().split())
-   c=sorted(list(map(int,input().split())))
+# for i in range(int(input())):
+#    a,b=map(int,input().split())
+#    c=sorted(list(map(int,input().split())))
 
-   d=[0]
+#    d=[0]
 
-   for _ in range(len(c)):
-      d.append(d[_]+c[_])
+#    for _ in range(len(c)):
+#       d.append(d[_]+c[_])
    
-   e=0
-   anss=10000000
-   for x in range(b-1,a):
-      e=d[x+1]-d[x-(b-1)]
-      t=c[x]*(b)-e
-      anss=min(anss,t)
+#    e=0
+#    anss=10000000
+#    for x in range(b-1,a):
+#       e=d[x+1]-d[x-(b-1)]
+#       t=c[x]*(b)-e
+#       anss=min(anss,t)
 
-   ans=[]
-   ans.append('Case #')
-   ans.append(str(i+1))
-   ans.append(': ')
-   ans.append(str(anss))
-   print(''.join(ans))
+#    ans=[]
+#    ans.append('Case #')
+#    ans.append(str(i+1))
+#    ans.append(': ')
+#    ans.append(str(anss))
+#    print(''.join(ans))
 
 
 
@@ -97,3 +97,60 @@ for i in range(int(input())):
 
 # for i,n in enumerate(digits):
 #     getBeautifulNumber(i+1,n)
+
+
+
+
+
+
+# t = int(input())
+# for i in range(1, t + 1):
+#    a = int(input())
+#    b = list(map(int, input().split()))
+#    c = 0
+#    for index in range(a-1):
+#       min_index = b.index(min(b[index:a]))
+#       b[index: min_index + 1] = reversed(b[index: min_index + 1])
+#       c += (min_index) - (index) + 1
+#    print("Case #{}: {}".format(i, c))
+
+# for _ in range(int(input())):
+#    a=list(map(int,input().split()))
+#    b=input()
+#    c=len(b)
+#    cnt=0
+#    ans=0
+#    for i in range(c):
+#       if(b[i]=='?'):
+         # cnt+=1
+#       if(cnt==c or cnt==c-1):
+#          print(0)
+#       for k in range(c-1):
+#          if(b[k]!='?'):
+#             break
+#       if(k==0):
+#          k+=1
+#       i=k
+#       while(i<c-1):
+#          if(b[i]=='?'):
+#             if(b[i-1]!=b[i+1] and b[i+1]!='?'):
+#                if(b[i-1]=='J'):
+#                   ans+=a[1]
+      
+#       ans=[]
+#       ans.append('Case #')
+#       ans.append(str(i+1))
+#       ans.append(': ')
+#       ans.append(str(cnt))
+#       print(''.join(ans))
+
+
+
+# a=[]
+# 4
+
+# 1 2 3 4
+# 4 2 1 3
+# 2 1 3 4
+
+
