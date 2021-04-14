e178cb6b2668bb140aa2f3ca0760bf110c77fc37
rahul1237 <rahulmahajan98645@gmail.com>
2021-04-15 03:50:35 +0530

    added

---
+++ sec-7(web-server)/public/js/app.js
@@ -0,0 +1,28 @@
+const weather_form = document.querySelector('form')
+const search = document.querySelector('input')
+const f_msg = document.querySelector('#f_para')
+const s_msg = document.querySelector('#s_para')
+
+// f_msg.textContent='rahul'
+
+weather_form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    const location = search.value
+    
+    f_msg.textContent='loading....'
+    s_msg.textContent=''
+
+    fetch('http://localhost:769/weather?search=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            f_msg.textContent=data.error
+        }else{
+            f_msg.textContent=data.area
+            s_msg.textContent='its currently ' + data.forecast.weather_type + ' in ' + data.area
+            console.log(data.location)
+            console.log('its currently ' + data.forecast.weather_type + ' in ' + data.area);
+        }
+    })
+    })
+
+})
