72c52776c7d356f9edc8f3a7b14acd8fc9ad5311
rahul1237 <rahulmahajan98645@gmail.com>
2021-04-24 03:20:26 +0530

    added

--- sec-10(task_manager)/mongodb.js
+++ sec-10(task_manager)/mongodb.js
@@ -13,5 +13,56 @@ mongoclient.connect(connectionUrl , {useUnifiedTopology: true} , (error,client) 
         return console.log('sorry unable to connect');
     }
 
-    console.log('connected correctly')
+    // console.log('connected correctly')
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'rahul',
+        age:19
+    }, (error,result) => {
+        if(error){
+            return console.log('inable to insert user');
+        }
+        console.log(result.ops);
+    })
+
+    db.collection('users').insertMany([
+        {
+            name:'yukti',
+            age:20,
+            pob:'delhi'
+        },{
+            name:'lala',
+            age:'20'
+        }
+    ], (error,result) => {
+        if(error){
+            return console.log('sorry unable to add data in users');
+        }
+
+        console.log(result.ops[0]);
+
+    })
+
+
+    db.collection('task').insertMany([
+        {
+            description: 'create notes',
+            completed:  false
+        },{
+            description: 'buy vegies',
+            completed:  true     
+        },{
+            description: 'message her',
+            completed:  true
+        }
+    ], (error,result) => {
+        if(error){
+            console.log('sorry unable to add data into task');
+        }
+
+        console.log(result.ops);
+    })
+
 })  
